ba3aa580ed9ca2f8616f919b78bbddefdf1b7b1d
DatumWorld <wk8614@163.com>
2026-03-29 10:17:05 +0800

    1 main.py修改为使用train_on_responses_only只对response算loss，因版本不能使用DataCollatorForCompletionOnlyLM 2 更新requirements.txt

--- demo/qwen_fine/main.py
+++ demo/qwen_fine/main.py
@@ -65,15 +65,8 @@ dataset = dataset.map(formatting_prompts_func, batched = True)
 
 
 
-from trl import SFTTrainer, DataCollatorForCompletionOnlyLM
+from trl import SFTTrainer
 from transformers import TrainingArguments
-
-# 1. 定义Response 起始符（必须与数据集中出现的文字完全一致），确保 Collator 能精准定位，否则可能失效
-response_template = "\n### Response:\n"
-collator = DataCollatorForCompletionOnlyLM(
-    response_template=response_template,
-    tokenizer=tokenizer
-)
 
 # 为2080ti 22GB 优化
 trainer = SFTTrainer(
@@ -81,7 +74,6 @@ trainer = SFTTrainer(
     tokenizer = tokenizer,
     train_dataset = dataset,
     dataset_text_field = "text",
-    data_collator = collator,  # <-- 核心改动：使用专门的 collator
     max_seq_length = 1024,
     args = TrainingArguments(
         per_device_train_batch_size = 1,
@@ -97,6 +89,17 @@ trainer = SFTTrainer(
         weight_decay = 0.01,
     ),
 )
+
+from unsloth.chat_templates import train_on_responses_only
+# 调用 Unsloth 专用包装器
+# 它会自动扫描 dataset 中的 "text" 列，并生成正确的 labels
+trainer = train_on_responses_only(
+    trainer,
+    instruction_part = "### Instruction:\n", # 掩码从此开始
+    response_part = "### Response:\n",       # 掩码到此结束（只对之后的内容算 Loss）
+)
+
+
 trainer.train()
 
 
